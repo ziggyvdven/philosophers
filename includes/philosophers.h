0aadd61624f4daf6411149ae03c61277e5352d63
ziggy <ziggyvandeven@gmail.com>
2023-06-15 13:54:25 -0400

    go

---
+++ includes/philosophers.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zvan-de- <zvan-de-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/15 13:43:36 by zvan-de-          #+#    #+#             */
+/*   Updated: 2023/06/15 13:53:19 by zvan-de-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include	"../libs/libft/includes/libft.h"
+# include	<stdio.h>
+# include	<stdlib.h>
+# include	<stdbool.h>
+# include	<limits.h>
+# include	<fcntl.h>
+
+
+typedef struct s_game
+{
+
+}				t_game;
+
+/*CHECKS*********************************/
+
+
+#endif
